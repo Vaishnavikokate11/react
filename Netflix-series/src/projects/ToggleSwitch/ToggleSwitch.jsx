17a8ab0264ed93d278bd8c4e8a78abd9a07ad1cf
Vaishnavikokate11 <vaishnavikokate55@gmail.com>
2025-10-06 11:12:13 +0530

    icons using react

--- Netflix-series/src/projects/ToggleSwitch/ToggleSwitch.jsx
+++ Netflix-series/src/projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./ToggleSwitch.css";
+import {IoIosSwitch} from "react-icons/io";
+import { FaUser } from "react-icons/fa";
 export const ToggleSwitch = () =>{
     const [isOn, setIsOn] = useState(false);
 
@@ -10,11 +12,15 @@ export const ToggleSwitch = () =>{
     const cheackSwitch = isOn ?  "On" : "Off";
     const toggleBG = {backgroundColor: isOn ?  "#4caf50" : "grey"}
     return (
+        <>
+        <h1 style={{color:"#000", textAlign:"center"}}>Toggle Switch <IoIosSwitch style={{color:"red", textAlign:"center"}}/> <FaUser /></h1>
         <div className="toggle-switch" 
         style={toggleBG} onClick={handleTOggleSwitch}>
+            
             <div className={`switch ${cheackSwitch}`}>
                 <span className="switch-state">{cheackSwitch}</span>
             </div>
         </div>
+        </>
     )
 }
